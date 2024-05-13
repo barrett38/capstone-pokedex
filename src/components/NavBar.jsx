beaf091ae698d8f3606082be65fb3f2ffffe5059
Barrett Anderson <barrett1614@gmail.com>
2024-05-13 09:06:54 -0500

    Removed Profile button if logged in

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,19 +4,9 @@ import speedIcon from "../assets/speedIcon.png";
 import enduranceIcon from "../assets/enduranceIcon.png";
 import shieldIcon from "../assets/shieldIcon.png";
 import LogInIcon from "../assets/LogInIcon.png";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "../store/authContext";
-import { NavLink } from "react-router-dom";
-
-const routes = [
-  { path: "/", icon: PokeballPic, alt: "Pokeball" },
-  { path: "/BestAttacks", icon: attackIcon, alt: "Attack" },
-  { path: "/BestSpeed", icon: speedIcon, alt: "Speed" },
-  { path: "/BestEndurance", icon: enduranceIcon, alt: "Endurance" },
-  { path: "/BestDefense", icon: shieldIcon, alt: "Defense" },
-  { path: "/auth", icon: LogInIcon, alt: "Log-in" },
-];
 
 export default function NavBar() {
   const { state, dispatch } = useContext(AuthContext);
@@ -26,6 +16,15 @@ export default function NavBar() {
       color: isActive ? "#f57145" : "",
     };
   };
+
+  const routes = [
+    { path: "/", icon: PokeballPic, alt: "Pokeball" },
+    { path: "/BestAttacks", icon: attackIcon, alt: "Attack" },
+    { path: "/BestSpeed", icon: speedIcon, alt: "Speed" },
+    { path: "/BestEndurance", icon: enduranceIcon, alt: "Endurance" },
+    { path: "/BestDefense", icon: shieldIcon, alt: "Defense" },
+    ...(state.token ? [] : [{ path: "/auth", icon: LogInIcon, alt: "Log-in" }]),
+  ];
 
   return (
     <header>
