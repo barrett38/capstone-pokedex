0cd83df65a5d823eed719c42e60fe486d838682b
Barrett Anderson <barrett1614@gmail.com>
2024-06-15 10:35:13 -0500

    Removed CORS

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 4005;
 const { sequelize } = require("./util/database");
@@ -15,7 +15,7 @@ const { register, login } = require("./controllers/auth");
 const { isAuthenticated } = require("./middleware/isAuthenticated");
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 Post.belongsTo(User);
 
